7c105fc0520e5025e893ecaf9387485c5fddf8c4
Rick van Gorp <rgorp@desktop-09.students.os3.nl>
2017-12-08 10:27:46 +0100

    Added run 3 and 4 support

--- parser/parser.py
+++ parser/parser.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 import numpy as np
 import os
 
-results = open("newres", "r")
+results = open("newresnew", "r")
 
 algorithms = ['cubic', 'ctcp', 'dctcp', 'bic', 'bbr']
 delays = [8, 80, 150, 220, 290]
@@ -16,7 +17,7 @@ def plot_graph(dataset, base_algo, sub_algo, loss, delay):
     sub_plot = {} 
     
     for result in dataset:
-        if dataset[result][0]["run"] == "1" or dataset[result][0]["run"] == "2": #Always combined same run in dict
+        if dataset[result][0]["run"] == "1" or dataset[result][0]["run"] == "2" or dataset[result][0]["run"] == "3" or dataset[result][0]["run"] == "4": #Always combined same run in dict
             #Yes, the if statement below is pretty useless. As of now. Not sure if this will have a purpose. 
             if "r" in result:
                 if dataset[result][0]["c"] == "1":
@@ -32,7 +33,7 @@ def plot_graph(dataset, base_algo, sub_algo, loss, delay):
                 elif dataset[result][0]["c"] == "2":
                     client1 = 1
                     client2 = 0
-
+            
             if len(dataset[result]) < 2:
                 print("Not complete error for algorithm: " + base_algo + " in combination with " + sub_algo) 
                 return False
@@ -43,9 +44,8 @@ def plot_graph(dataset, base_algo, sub_algo, loss, delay):
     
     if "1" not in base_plot:
         return False
-
-    print("Bandwidth points " + base_algo +  " per 1 second: " + str(base_plot))
-    print("Bandwidth points " + sub_algo + " per 1 second: " + str(sub_plot)) 
+    #print("Bandwidth points " + base_algo +  " per 1 second: " + str(base_plot))
+    #print("Bandwidth points " + sub_algo + " per 1 second: " + str(sub_plot)) 
     base_y = np.array(base_plot["1"])
     sub_y = np.array(sub_plot["1"])
     base_algor, = plt.plot(base_y, label=base_algo, color="red")
@@ -54,13 +54,28 @@ def plot_graph(dataset, base_algo, sub_algo, loss, delay):
     if len(base_plot) > 1 and len(sub_plot) > 1:
         base_y_2 = np.array(base_plot["2"])
         sub_y_2 = np.array(sub_plot["2"])
-        print(len(base_plot["1"]))
-        plot_height = max(max(base_plot["1"]), max(sub_plot["1"]), max(base_plot["2"]), max(sub_plot["2"])) + 1000
+        if len(base_plot) > 2:
+            print("YES")
+        if len(base_plot) == 4 and len(sub_plot) == 4:
+            print("Should print 2 extra by now")
+            base_y_3 = np.array(base_plot["3"])
+            sub_y_3 = np.array(sub_plot["3"])
+            base_y_4 = np.array(base_plot["4"])
+            sub_y_4 = np.array(sub_plot["4"])
+            base_algo_3, = plt.plot(base_y_3, label=base_algo + "extra")
+            sub_algo_3, = plt.plot(sub_y_3, label=sub_algo + "extra")
+            base_algo_4, = plt.plot(base_y_4, label=base_algo + "extra2")
+            sub_algo_4, = plt.plot(sub_y_4, label=sub_algo + "extra2")
+        else:    
+            plot_height = max(max(base_plot["1"]), max(sub_plot["1"]), max(base_plot["2"]), max(sub_plot["2"])) + 1000
         label1 = base_algo + " reverse"
         label2 = sub_algo + " reverse"
         base_algo_2, = plt.plot(base_y_2, label=label1, color="purple")
         sub_algo_2, = plt.plot(sub_y_2, label=label2, color="lightgreen")
-        plt.legend(handles=[base_algor, sub_algor, base_algo_2, sub_algo_2])
+        if len(base_plot) == 4 and len(sub_plot) == 4:
+            plt.legend(handles=[base_algor, sub_algor, base_algo_2, sub_algo_2, base_algo_3, sub_algo_3, base_algo_4, sub_algo_4])
+        else:
+            plt.legend(handles=[base_algor, sub_algor, base_algo_2, sub_algo_2])
     else:
         plt.legend(handles=[base_algor, sub_algor])
     plt.ylabel("Bytes sent")
@@ -96,10 +111,7 @@ for delay in delays:
             for algo2 in algorithms:
                 if algo1 == algo2:
                     continue
-
                 try:
                     plot_graph(result_data, algo1, algo2, str(loss), str(delay))
                 except:
                     pass
-
-
